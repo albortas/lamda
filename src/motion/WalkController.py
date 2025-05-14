f7bf2091e61f9419b51de432245756f0fff56511
albortas <garlad628@gmail.com>
2025-05-14 17:00:57 -0400

    remember CenterMass

--- src/motion/WalkController.py
+++ src/motion/WalkController.py
@@ -1,7 +1,7 @@
 import numpy as np
 from math import sin, cos
 
-from src.cmass.CMass import CMass
+from src.cmass.CenterMass import CenterMass
 from src.kinematics.kinematics import inverse_kinematics_all
 from src.utils.transformations import xyz_rotation_matrix, new_coordinates
 from src.motion.UpdateMovement import UpdateMovement
@@ -12,7 +12,7 @@ class WalkGaitController():
     def __init__(self, x_offset, default_height, legs, default_stance, default_frame):
         self.legs = legs
         self.CG = np.zeros(3)
-        self.cmass = CMass(default_frame, legs)
+        self.centerMass = CenterMass(default_frame, legs)
         self.updateMovement = UpdateMovement(default_stance)
         self.crawlController = CrawlController(x_offset, default_height, self.CG, default_stance, default_frame)
         
@@ -25,11 +25,11 @@ class WalkGaitController():
         state.center = [state.center_map[0]+(xc*cos(state.theta[2])-yc*sin(state.theta[2])),
                         state.center_map[1]+(xc*sin(state.theta[2])+yc*cos(state.theta[2]))]
         state.angles_foot = inverse_kinematics_all(state.foot_position,self.legs)
-        self.CG = self.cmass.CM_calculation_o(state.angles_foot)
+        self.CG = self.centerMass.CM_calculation_o(state.angles_foot)
         M = xyz_rotation_matrix(state.theta[0], state.theta[1], state.theta[2])
         state.CGabs = new_coordinates(M, self.CG[0], self.CG[1], self.CG[2],
                                       state.framecenter_comp[0], state.framecenter_comp[1], state.framecenter_comp[2])
-        state.dCG = self.cmass.CG_distance(state.foot_abs[0], state.foot_abs[1], state.foot_abs[2], state.CGabs[0], state.CGabs[1], state.stance)
+        state.dCG = self.centerMass.CG_distance(state.foot_abs[0], state.foot_abs[1], state.foot_abs[2], state.CGabs[0], state.CGabs[1], state.stance)
         print(self.CG)
         
     def run(self, state, command):
